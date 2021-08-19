01237fd06eee7eb899320e1bf91d1fdfb36b287c
sakshamlamba109 <500075568@stu.upes.ac.in>
2021-08-19 22:43:11 +0530

    changes

--- personality_predictor/lib/main.dart
+++ personality_predictor/lib/main.dart
@@ -3,18 +3,8 @@ import 'package:flutter/material.dart';
 void main() => runApp(PersonalityChecker());
 
 class PersonalityChecker extends StatelessWidget {
-
-void answer()
-{
-  print('Answer chosen'),
-
-}
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favourite colour?',
-      'What\'s your favourite food',
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -23,15 +13,15 @@ void answer()
         body: Column(
           children: [
             ElevatedButton(
-              onPressed: answer,
+              onPressed: () {},
               child: Text('Answer 1'),
             ),
             ElevatedButton(
-              onPressed: answer,
+              onPressed: () {},
               child: Text('Answer 2'),
             ),
             ElevatedButton(
-              onPressed: answer,
+              onPressed: () {},
               child: Text('Answer 3'),
             ),
           ],
